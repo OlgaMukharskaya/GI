4c207552e970763d1ac7dd5a05b437fbf2e3272a
Olga Mukharskaya <olgamukharskaya@coherentsolutions.com>
2022-03-10 23:42:29 +0300

    Task-2 index quarters

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -6,7 +6,19 @@ namespace HomeWork
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] Array = new int[10];
+            for (int index = 0; index < Array.Length; index++)
+            {
+                Array[index] = index * index;
+            }
+
+            foreach (int item in Array)
+
+            {
+                Console.WriteLine(item);
+            }
+
+            //Создать массив размером 10 элементов, заполнить его квадратами индексов (в цикле), и вывести его содержимое на экран
         }
     }
 }
