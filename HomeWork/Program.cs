0c083c928818f31f73055fc1400bf372d69bbe5b
Olga Mukharskaya <olgamukharskaya@coherentsolutions.com>
2022-03-13 21:44:11 +0300

    Task-3 create 3 arrays

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -1,12 +1,57 @@
 ﻿using System;
 
-namespace HomeWork
+namespace ConsoleApp2
 {
     class Program
     {
+
+        static void InitializeArray(int[] array, int coefficient)
+        {
+            for (int index = 0; index < array.Length; index++)
+            {
+                array[index] = Convert.ToInt32(Math.Pow(index, coefficient));
+            }
+        }
+
+        static double GetAverage(int[] Array)
+        {
+            double sum = 0;
+            for (int index = 0; index < Array.Length; index++)
+            {
+                sum = sum + Array[index];
+            }
+            double average = Array.Length != 0 ? (double)sum / Array.Length : 0;
+
+            return sum / Array.Length;
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] Array1 = new int[10];
+            InitializeArray(Array1, 1);
+            double ArrayAverage1 = GetAverage(Array1);
+
+
+            int[] Array2 = new int[10];
+            InitializeArray(Array2, 2);
+            double ArrayAverage2 = GetAverage(Array2);
+
+            int[] Array3 = new int[10];
+            InitializeArray(Array3, 3);
+            double ArrayAverage3 = GetAverage(Array3);
+
+
+
+            Console.WriteLine(ArrayAverage1);
+            Console.WriteLine(ArrayAverage2);
+            Console.WriteLine(ArrayAverage3);
+
+
         }
     }
 }
+
+
+// Cоздать три массива по 10 элементов, первый заполнить значениями индексов элементов, второй - квадратами индексов, третий-кубами, и вывести на экран средние значения элементов для всех массивов
+
