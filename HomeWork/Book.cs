d903e8d66c3ec429cce66a42f10a560c5d1918a0
Olga Mukharskaya <olgamukharskaya@coherentsolutions.com>
2022-03-18 14:52:56 +0300

    Task-5 Books (Class)

---
+++ HomeWork/Book.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork
+{
+    class Book
+    {
+
+        public string Title { get; }
+        public int Pages { get; }
+        public string ID { get; }
+
+
+        public Book(string title, int pages, string id)
+        {
+            Title = title;
+            Pages = pages;
+            ID = id;
+        }
+
+        public string GetBookInfo()
+        {
+            return String.Format("Title - {0}, Number of pages - {1}, BookID - {2}", Title, Pages, ID);
+        }
+
+
+    }
+
+}
+
--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 
-namespace ConsoleApp5
+namespace HomeWork
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Book Book1 = new Book("BookName-1", 11, "8bde16de-7ce5");
+            Book Book2 = new Book("BookName-2", 222, "e062cacd-a56c");
+            Book Book3 = new Book("BookName-3", 3, "061ab441-9f71");
+            Book Book4 = new Book("BookName-4", 44, "23344637-0d35");
+            Book Book5 = new Book("BookName-5", 555, "137c348e-56d5");
+
+            Book[] Books = { Book1, Book2, Book3, Book4, Book5 };
+
+            foreach (Book item in Books)
+            {
+                Console.WriteLine(item.GetBookInfo());
+            }
+
+            //создать класс Книга (в нем название (строка), количество страниц (int), уникальный идентификатор (строка)..
+            //Создайте массив книг (5 штук) проинициализируйте его различными объектами типа "Книга",
+            //и выведите информацию о всех книгах в массиве на экран
+
         }
     }
 }
