54f40d46b3725aab03c4ffa3679ec1decdfc4912
Olga Mukharskaya <olgamukharskaya@coherentsolutions.com>
2022-04-06 22:17:15 +0300

    Task-4 Variant-2

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -1,56 +1,72 @@
 ﻿using System;
 
-namespace ConsoleApp2
+
+namespace Task_4
 {
     class Program
     {
 
-        static void ChangeMinMaxValues(int[] Array)
+        public Array GetMinMaxValues(int[] array)
         {
-            int minElementValue = Array[0];
-            int minElementIndex = 0;
-            int maxElementValue = Array[0];
-            int maxElementIndex = 0;
+            int MinValue = array[0];
+            int MaxValue = array[0];
+            int[] Array = { MinValue, MaxValue };
 
-            for (int index = 0; index < Array.Length; index++)
+
+            for (int i = 0; i < array.Length; i++)
             {
-                int temp = Array[index];
-                if (temp > maxElementValue)
-                {
-                    maxElementValue = temp;
-                    maxElementIndex = index;
-                }
-
-                if (temp < minElementValue)
-                {
-                    minElementValue = temp;
-                    minElementIndex = index;
-                }
+                if (array[i] < MinValue) MinValue = array[i];
+                if (array[i] > MaxValue) MaxValue = array[i];
             }
 
-            Console.WriteLine("Min Element Value: {0}, Max Element Value: {1} ", minElementValue, maxElementValue);
-
-            Array[minElementIndex] = maxElementValue;
-            Array[maxElementIndex] = minElementValue;
-
+            return Array;
         }
 
 
+        static void ChangeMinMaxValues(int[] Array)
+        {
+            int MaxValue = Array[0]; int MaxValueIndex = 0;
+            int MinValue = Array[0]; int MinValueIndex = 0;
+
+            for (int index = 0; index < Array.Length; index++)
+            {
+                if (Array[index] > MaxValue)
+                {
+                    MaxValue = Array[index];
+                    MaxValueIndex = index;
+                }
+                if (Array[index] < MinValue)
+                {
+                    MinValue = Array[index];
+                    MinValueIndex = index;
+                }
+            }
+
+            Array[MinValueIndex] = MaxValue;
+            Array[MaxValueIndex] = MinValue;
+
+        }
 
         static void Main(string[] args)
         {
+
+            Program array = new Program();
+
             int[] Array1 = { 2, 5, 90, 45, -1, 77 };
             int[] Array2 = { 77, -5, 45, 0, 99, 5, 2 };
+
+
+            Console.WriteLine(array.GetMinMaxValues(Array1));
+            Console.WriteLine(array.GetMinMaxValues(Array2));
 
 
             ChangeMinMaxValues(Array1);
             ChangeMinMaxValues(Array2);
 
+
+            //Console.WriteLine("Numbers in Array: \n{0}", string.Join("\n", Array1));
+            //Console.WriteLine("Numbers in Array: \n{0}", string.Join("\n", Array2));
+
         }
     }
 }
-
-
-// Создать два произвольных массива, подсчитать максимальное и минимальное значение их элементов. Вывести на экран
-// Поменять в каждом массиве максимальные и минимальные значения местами
-
